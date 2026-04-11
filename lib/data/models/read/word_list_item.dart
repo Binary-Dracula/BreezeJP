cc2257ad1d9f094b5875a42a5098b41f05927366
summer <summer.work.001@gmail.com>
2026-04-11 23:12:52 +0800

    refactor: remove legacy statistics and island learning flow

--- lib/data/models/read/word_list_item.dart
+++
@@ -1,9 +0,0 @@
-import '../word.dart';
-
-/// 单词列表项（包含主释义）
-class WordListItem {
-  final Word word;
-  final String? primaryMeaning;
-
-  const WordListItem({required this.word, this.primaryMeaning});
-}
--- lib/data/repositories/daily_stat_repository_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../db/app_database.dart';
-import 'daily_stat_repository.dart';
-
-/// DailyStatRepository Provider
-/// 提供全局单例的每日统计数据仓库
-final dailyStatRepositoryProvider = Provider<DailyStatRepository>((ref) {
-  return DailyStatRepository(() => AppDatabase.instance.database);
-});
